f0019a78e96c949249ae55ad3dc50e74428290ee
Ralph Giles <giles@mozilla.com>
2013-03-01 21:54:43 -0800

    Simple node+redis script to report a hit counter.

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require("http");
+var redis = require("redis").createClient();
+
+redis.debug_mode = true;
+
+redis.on('error', function(err) {
+  console.log("redis error: " + err);
+});
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+
+  console.log('connection ' + request.connection.remoteAddress);
+  redis.incr('hello:requests', function(err, reply) {
+    console.log('got redis reply ' + reply.toString());
+    console.log('    error is ' + err);
+    response.write('Hello ' + reply + ' from node.js.\n');
+    response.end();
+  });
+
+}).listen(8888);
